5359032a22b0a470a4e4e35d182796471ce4f415
Daniel Libanori <daniellibanori@gmail.com>
2015-12-15 13:05:10 -0200

    Transfor command into a module

--- lib/clicksign/command.rb
+++ lib/clicksign/command.rb
@@ -1,13 +1,4 @@
 module Clicksign
-  class Command
-    attr_accessor :resource
-
-    def initialize(resource)
-      @resource = resource
-    end
-
-    def get(extras = {})
-      resource.get(extras)
-    end
+  module Command
   end
 end
