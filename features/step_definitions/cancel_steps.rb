4bf04ff71cf190ea29b7c578c63733ce84130d14
Daniel Libanori <daniellibanori@gmail.com>
2015-12-17 15:51:30 -0200

    Add cancel steps

---
+++ features/step_definitions/cancel_steps.rb
@@ -0,0 +1,11 @@
+Dado(/^que há um documento sendo assinado$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando(/^cancelo o documento$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então(/^os signatários que já assinaram não notificados$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
