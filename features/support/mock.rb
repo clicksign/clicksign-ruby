21fb227eba2bddfee4a8b8bb9ee224d30e2c61f4
Daniel Libanori <daniellibanori@gmail.com>
2015-12-21 21:22:50 -0200

    Improve features mock

--- features/support/mock.rb
+++ features/support/mock.rb
@@ -4,33 +4,39 @@ require 'webmock/cucumber'
 Before do
   document = { document: { key: '17' }}
 
-  stub_request(:get, 'http://example.com/whoami?access_token=valid').
+  stub_request(:get, 'http://example.com/whoami').
+    with(query: { access_token: 'valid' }).
     to_return(status: 200, body: nil)
 
-  stub_request(:get, 'http://example.com/whoami?access_token=invalid').
+  stub_request(:get, 'http://example.com/whoami').
+    with(query: { access_token: 'invalid' }).
     to_return(status: 401, body: nil)
 
-  stub_request(:get, 'http://example.com/documents?access_token=empty').
+  stub_request(:get, 'http://example.com/documents').
+    with(query: { access_token: 'empty' }).
     to_return(status: 200, body: [].to_json)
 
-  stub_request(:get, 'http://example.com/documents?access_token=some').
+  stub_request(:get, 'http://example.com/documents').
+    with(query: { access_token: 'some' }).
     to_return(status: 200, body: [document, document].to_json)
 
-  stub_request(:post, 'http://example.com/documents?access_token=token').
+  stub_request(:post, 'http://example.com/documents').
+    with(query: { access_token: 'token' }).
     to_return(status: 200, body: { key: '17' }.to_json)
 
-  stub_request(:post, 'http://example.com/documents/17/cancel?access_token=token').
+  stub_request(:post, 'http://example.com/documents/17/cancel').
+    with(query: { access_token: 'token' }).
     to_return(status: 200, body: { document: { key: '17', list: { canceled_at: 'now' }}}.to_json)
 
-  stub_request(:get, 'http://example.com/documents/17/download?access_token=token').
+  stub_request(:get, 'http://example.com/documents/17/download').
+    with(query: { access_token: 'token' }).
     to_return(status: 200, body: 'content...')
 
-  # stub_request(:post, 'http://example.com/documents/coffee').
-  stub_request(:get, 'http://example.com/documents/coffee?access_token=token').
-    # with(query: { access_token: 'token' }).
+  stub_request(:get, 'http://example.com/documents/coffee').
+    with(query: { access_token: 'token' }).
     to_return(status: 200, body: { document: { key: '17' }}.to_json)
 
-  stub_request(:get, 'http://example.com/documents/invalid?access_token=token').
-    # with(query: { access_token: 'token' }).
+  stub_request(:get, 'http://example.com/documents/invalid').
+    with(query: { access_token: 'token' }).
     to_return(status: 500, body: nil)
 end
